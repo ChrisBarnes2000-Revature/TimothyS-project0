422912cbf049872ace64a09a568d7a38a5cf56e2
TSim77 <TimSim777@gmail.com>
2022-06-12 22:24:45 -0500

    Added temporary dictionaries of medications to test, somehow implementation is still not pefected.

--- MedReminder.py
+++ MedReminder.py
@@ -1,6 +1,14 @@
 from calendar import week
 from calendar import timegm
 import datetime
+from operator import itemgetter
+
+# dicts holding medications and prescribed date & time (for now until creating separate module)
+userMedsMonday = {0800: 'Pill A', 2000: 'Pill B', 0800: 'Pill C', 2000: 'Pill D'}
+userMedsTuesday = {0800: 'Pill E', 1800: 'Pill D'}
+userMedsWednesday = {0800: 'Pill A', 2000: 'Pill B', 0800: 'Pill C', 2000: 'Pill D'}
+userMedsThursday = {0800: 'Pill E', 1800: 'Pill D'}
+userMedsFriday = {0800: 'Pill A', 2000: 'Pill B', 0800: 'Pill C', 2000: 'Pill D'}
 
 # Medication reminder program by Timothy S.
 # Purpose is to remind user of what medications to take at specific time on the day of the week
@@ -16,3 +24,8 @@ print('Now searching for prescribed medications on ' +
       current_day + ' at ' + current_time)
 
 # Searching for medications to be taken at inputted day and time
+if current_day == 'Monday':
+    if current_time >= 0800:
+        print(userMedsMonday.get(0800))
+    elif current_time >= 2000:
+        print(userMedsMonday.get(2000))
