5679fab107746df6208c285d38222c028f62d486
TSim77 <TimSim777@gmail.com>
2022-06-14 11:25:15 -0500

    Modification of algorithm and medication database into string dictionary to get code working.

--- MedReminder.py
+++ MedReminder.py
@@ -2,6 +2,7 @@ from calendar import week
 from calendar import timegm
 import datetime
 from operator import itemgetter
+from Medications import get_meds
 
 # dicts holding medications and prescribed date & time (for now until creating separate module)
 # userMedsMonday = {0800: 'Pill A', 2000: 'Pill B', 0800: 'Pill C', 2000: 'Pill D'}
@@ -16,15 +17,17 @@ from operator import itemgetter
 days_of_the_week = ['Monday', 'Tuesday', 'Wednesday',
                     'Thursday', 'Friday', 'Saturday', 'Sunday']
 
+# Start of programMon
 print('Hello and welcome to your medication reminder!')
 
 # Input day of the week and current time
 current_day = str(input('Please type the current day of the week: '))
-current_time = input(
-    'Please enter the current time in 24-hour format: ')
+current_time = str(input(
+    'Please enter the current time in 24-hour format: '))
 
 # Searching for medications to be taken at inputted day and time
 for day in days_of_the_week:
     if current_day == day:
         print('Here are your prescribed medications for ' +
               day + ' at ' + current_time + ":")
+        print(get_meds(current_day, current_time))
--- Medications.py
+++ Medications.py
@@ -3,19 +3,35 @@ from calendar import timegm
 import datetime
 from operator import itemgetter
 
+
 days_of_the_week = ['Monday', 'Tuesday', 'Wednesday',
                     'Thursday', 'Friday', 'Saturday', 'Sunday']
-# Data Structures containing prescribed medications and their day and time to be taken
-# meds = {Med A: {days: ["Monday", "Wednesday", "Friday"], time: "0800"}, Med B: {days: ["Monday", "Wednesday"], time: "0800"}, Med C: {days: ["Tuesday, Thursday"], time: "0800"}
-#        }
-monday_meds = {(Pill A, Pill B, Pill C): 0800, (Pill A, Pill D): 1800}
-tuesday_meds = {(Pill E, Pill F): 0800, (Pill E): 1800}
-wednesday_meds = {(Pill A, Pill B, Pill C): 0800, (Pill A, Pill D): 1800}
-thursday_meds = {(Pill E, Pill F): 0800, (Pill E): 1800}
-friday_meds = {(Pill A, Pill B, Pill C): 0800, (Pill A, Pill D): 1800}
-saturday_meds = {(Pill G): 0900, (Pill H): 2100}
-sunday_meds = {(Pill G): 0900, (Pill H): 2100}
+
+# Data Structures containing prescribed medications and their day and time to be taken (Time to take medication: Medications prescribed)
+monday_meds = {'800': 'Pill A, Pill B, Pill C', '1800': 'Pill A, Pill D'}
+tuesday_meds = {'800': 'Pill E, PIll F', '1800': 'Pill E'}
+wednesday_meds = {'800': 'Pill A, Pill B, Pill C', '1800': 'Pill A, Pill D'}
+thursday_meds = {'800': 'Pill E, Pill F', '1800': 'Pill E'}
+friday_meds = {'800': 'Pill A, Pill B, Pill C', '1800': 'Pill A, Pill D'}
+saturday_meds = {'800': 'Pill G', '2100': 'Pill H'}
+sunday_meds = {'800': 'Pill G', '2100': 'Pill H'}
 
 
+# Function that returns the prescribed medications on the given day and time
 def get_meds(day, time):
-    pass
+    for x in days_of_the_week:
+        if day == x:
+            if x == 'Monday':
+                return monday_meds.get(time)
+            elif x == 'Tuesday':
+                return tuesday_meds.get(time)
+            elif x == 'Wednesday':
+                return wednesday_meds.get(time)
+            elif x == 'Thursday':
+                return thursday_meds.get(time)
+            elif x == 'Friday':
+                return friday_meds.get(time)
+            elif x == 'Saturday':
+                return saturday_meds.get(time)
+            elif x == 'Sunday':
+                return sunday_meds.get(time)
