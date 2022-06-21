cea079da99479fcf9f58879a133af5158baf8216
Christopher Barnes <chris.barnes.2000@me.com>
2022-06-21 00:15:05 -0700

    Compress Test Dict OF Lizzy's Meds

--- Clients/TestClients/Lizzy.py
+++ Clients/TestClients/Lizzy.py
@@ -1,23 +1,9 @@
 Lizzy_MedList = {
-    "MONDAY": {
-        "1200": {"Multi-Vitamins": "More Info To Come"}
-    },
-    "TUSEDAY": {
-        "1200": {"Multi-Vitamins": "More Info To Come"}
-    },
-    "WEDNESDAY": {
-        "1200": {"Multi-Vitamins": "More Info To Come"}
-    },
-    "THURSDAY": {
-        "1200": {"Multi-Vitamins": "More Info To Come"}
-    },
-    "FRIDAY": {
-        "1200": {"Multi-Vitamins": "More Info To Come"}
-    },
-    "SATURDAY": {
-        "1200": {"Multi-Vitamins": "More Info To Come"}
-    },
-    "SUNDAY": {
-        "1200": {"Multi-Vitamins": "More Info To Come"}
-    }
+    "SUNDAY":    {"1200": {"Multi-Vitamins": "More Info To Come"}},
+    "MONDAY":    {"1200": {"Multi-Vitamins": "More Info To Come"}},
+    "TUSEDAY":   {"1200": {"Multi-Vitamins": "More Info To Come"}},
+    "WEDNESDAY": {"1200": {"Multi-Vitamins": "More Info To Come"}},
+    "THURSDAY":  {"1200": {"Multi-Vitamins": "More Info To Come"}},
+    "FRIDAY":    {"1200": {"Multi-Vitamins": "More Info To Come"}},
+    "SATURDAY":  {"1200": {"Multi-Vitamins": "More Info To Come"}},
 }
