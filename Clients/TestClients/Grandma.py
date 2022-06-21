ad5dac3d27c43a214070a3f90abfddecffe4d2d8
Christopher Barnes <chris.barnes.2000@me.com>
2022-06-20 17:55:06 -0700

    Add Test Users Medlists

---
+++ Clients/TestClients/Grandma.py
@@ -0,0 +1,31 @@
+Grandma_MedList = {
+    "SUNDAY": {
+        "1200": {}
+    },
+    "MONDAY": {
+        "0600": {"pill_a": "More Info To Come"},
+        "1200": {"pill_b": "More Info To Come"},
+        "1800": {"pill_c": "More Info To Come"}
+    },
+    "TUSEDAY": {
+        "1200": {"Multi-Vitamins": "More Info To Come"},
+        "1800": {"Multi-Vitamins": "More Info To Come"}
+    },
+    "WEDNESDAY": {
+        "0600": {"pill_a": "More Info To Come"},
+        "1200": {"pill_b": "More Info To Come"},
+        "1800": {"pill_c": "More Info To Come"}
+    },
+    "THURSDAY": {
+        "1200": {"Multi-Vitamins": "More Info To Come"},
+        "1800": {"Multi-Vitamins": "More Info To Come"}
+    },
+    "FRIDAY": {
+        "0600": {"pill_a": "More Info To Come"},
+        "1200": {"pill_b": "More Info To Come"},
+        "1800": {"pill_c": "More Info To Come"}
+    },
+    "SATURDAY": {
+        "1200": {}
+    }
+}
